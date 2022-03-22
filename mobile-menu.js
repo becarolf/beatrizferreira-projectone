545b185f135f89ef45d17d4901bda3b02a33ebd9
Beatriz <becarolf@gmail.com>
2022-03-22 00:15:30 -0400

    adjusted blog/home styles and created nav mobile

---
+++ mobile-menu.js
@@ -0,0 +1,9 @@
+function toggleMenu() {
+    console.log("HERE")
+	const x = document.getElementById("mobileMenu");
+	if (x.style.display === "block") {
+		x.style.display = "none";
+	} else {
+		x.style.display = "block";
+	}
+}
